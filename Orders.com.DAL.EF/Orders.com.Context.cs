4cfdda43250d212fc1774adeec38ee31985d55b5
unknown <aaron_hanusa@hotmail.com>
2015-11-26 21:52:55 -0600

    Add Orders.com.DAL.EF project

---
+++ Orders.com.DAL.EF/Orders.com.Context.cs
@@ -0,0 +1,15 @@
+﻿using Orders.com.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.com.DAL.EF
+{
+	public class OrdersDotComContext : DbContext
+    {
+		public DbSet<Customer> Customers { get; set; }
+    }
+}
