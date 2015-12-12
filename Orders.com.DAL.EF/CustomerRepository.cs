87e51eaecb4f0f6d5b4a111444f1e863a41a2cc4
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-12-11 21:39:50 -0600

    remove MVC and EF projects

--- Orders.com.DAL.EF/CustomerRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Orders.com.Domain;
-using Orders.com.DataProxy;
-
-namespace Orders.com.DAL.EF
-{
-    public class CustomerRepository : OrdersDotComRepositoryBase<Customer>, ICustomerDataProxy
-    {
-    }
-}
--- Orders.com.Web.MVC/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Orders.com.Web.MVC.MvcApplication" Language="C#" %>
