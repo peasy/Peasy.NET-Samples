79c1ad27e549fcf418521a1fcc75e1a033d84458
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-12-20 12:15:06 -0600

    Add MVC project

---
+++ Orders.com.Web.MVC/Configuration/DIConfigSection.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace Orders.com.Web.MVC.Configuration
+{
+    public class DIConfigSection : ConfigurationSection
+    {
+        [ConfigurationProperty("bindings")]
+        public DIConfigList Bindings
+        {
+            get { return (DIConfigList)base["bindings"]; }
+        }
+    }
+}
---
+++ Orders.com.Web.MVC/ViewModels/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Orders.com.Web.MVC.ViewModels
+{
+    public class ViewModel<T>
+    {
+        public T Entity { get; set; }
+        public IEnumerable<ValidationResult> Errors { get; set; } = Enumerable.Empty<ValidationResult>();
+    }
+}
