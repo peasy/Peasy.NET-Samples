9b515dcaef8cb4d2975c9136c178c65ab6019241
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-12-24 14:23:13 -0600

    Add inventory functionality

---
+++ Orders.com.Web.MVC/Controllers/InventoryItemsController.cs
@@ -0,0 +1,53 @@
+﻿using Orders.com.BLL.Domain;
+using Orders.com.BLL.Services;
+using Orders.com.Web.MVC.ViewModels;
+using Peasy.Exception;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Orders.com.Web.MVC.Controllers
+{
+    public class InventoryItemsController : ControllerBase<InventoryItem, InventoryItemViewModel>
+    {
+        private IProductService _productService;
+        private IEnumerable<Product> _products;
+
+        public InventoryItemsController(IInventoryItemService service, IProductService productService) : base(service)
+        {
+            _productService = productService;
+        }
+
+        protected override InventoryItemViewModel ConfigureVM(InventoryItemViewModel vm)
+        {
+            vm.ProductName = Products.First(p => p.ProductID == vm.ProductID).Name;
+            return vm;
+        }
+
+        public override ActionResult Edit(InventoryItemViewModel vm)
+        {
+            try
+            {
+                return base.Edit(vm);
+            }
+            catch (ConcurrencyException ex)
+            {
+                vm.Errors.Add(new ValidationResult(ex.Message));
+                return View(vm);
+            }
+        }
+
+        private IEnumerable<Product> Products
+        {
+            get
+            {
+                if (_products == null)
+                {
+                    _products = _productService.GetAllCommand().Execute().Value;
+                }
+                return _products;
+            }
+        }
+    }
+}
---
+++ Orders.com.Web.MVC/ViewModels/InventoryItemViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Orders.com.BLL.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Orders.com.Web.MVC.ViewModels
+{
+    public class InventoryItemViewModel : ViewModel<InventoryItem>
+    {
+        public long ID
+        {
+            get { return Entity.ID; }
+            set { Entity.ID = value; }
+        }
+
+        [Display(Name ="Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name ="Quantity")]
+        public decimal QuantityOnHand
+        {
+            get { return Entity.QuantityOnHand; }
+            set { Entity.QuantityOnHand = value; }
+        }
+
+        public long ProductID
+        {
+            get { return Entity.ProductID; }
+            set { Entity.ProductID = value; }
+        }
+
+        public string Version
+        {
+            get { return Entity.Version; }
+            set { Entity.Version = value; }
+        }
+    }
+}
