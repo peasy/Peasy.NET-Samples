bd5442538cdef0ea0e626d12feb52a6d985b5675
Aaron Hanusa <aaron.hanusa@gmail.com>
2015-12-25 11:22:10 -0600

    First pass at Orders functionality

---
+++ Orders.com.Web.MVC/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Orders.com.BLL.Domain;
+using Orders.com.BLL.Services;
+using Orders.com.Web.MVC.ViewModels;
+
+namespace Orders.com.Web.MVC.Controllers
+{
+    public class OrdersController : ControllerBase<Order, OrderViewModel>
+    {
+        public OrdersController(IOrderService service) : base(service)
+        {
+        }
+
+        public override ActionResult Index(string search)
+        {
+            var service = _service as IOrderService;
+            var results = service.GetAllCommand(0, 100).Execute().Value;
+            return View(results);
+        }
+    }
+}
---
+++ Orders.com.Web.MVC/ViewModels/OrderViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Orders.com.BLL.Domain;
+
+namespace Orders.com.Web.MVC.ViewModels
+{
+    public class OrderViewModel : ViewModel<Order>
+    {
+    }
+}
