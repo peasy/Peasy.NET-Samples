b4d38659befd1f0c6dc7f359d8cd77b28a56eec3
Aaron Hanusa <aaron_hanusa@hotmail.com>
2017-06-24 18:50:25 -0500

    Add Orders.com.Web.MVC.Core

---
+++ Orders.com.Web.MVC.Core/src/Orders.com.Web.MVC.Core/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.IdentityModel.Protocols;
+
+namespace Orders.com.Web.MVC.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = new ConfigurationManager();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
